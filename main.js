c035899536b0e40b7b9dc13c375307dc6030c029
Dante <42275387+TJANGEL@users.noreply.github.com>
2019-03-18 18:00:58 -0400

    added same functionality using js

---
+++ main.js
@@ -0,0 +1,21 @@
+// jQuery Smooth Scroll
+// $(".navbar a").on("click", function(e) {
+//   console.log(this.hash);
+//   if (this.hash !== "") {
+//     e.preventDefault();
+
+//     const hash = this.hash;
+
+//     $("html", "body").animate(
+//       {
+//         scrollTop: $(hash).offset().top
+//       },
+//       800
+//     );
+//   }
+// });
+
+// Smooth Scroll cdn -- https:github.com/cferdinandi/smooth-scroll;
+const scroll = new SmoothScroll('.navbar a[href*="#"]', {
+  speed: 800
+});
